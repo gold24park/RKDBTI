8fd75cfbe840fe75c4c9bdc4cd9bfc0a30ab72ab
lx5475 <lx5475@gmail.com>
2022-06-22 02:33:46 +0900

    DB connection atomic 하게 변경

--- src/pages/api/result.ts
+++ src/pages/api/result.ts
@@ -5,61 +5,78 @@ import { Filter, FindOptions } from "mongodb";
 import { NextApiResponse } from "next";
 import nc from "next-connect";
 
-const handler = nc()
+const handler = nc();
 
 // 가장 높은 점수를 받은 항목의 데이터를 가져옵니다.
-handler.use(getSession).use(getDatabase).get<DatabaseRequest, NextApiResponse>(async (req, res) => {    
-    let typeNumber = parseInt(req.query.typeNumber as string, 0)
-    
-    let character: Character | null | undefined = await req.db.characters?.findOne({
-        unique_id: typeNumber as number
-    } as Filter<Character>, 
-    {projection: {_id: 0}} as FindOptions)
+handler
+  .use(getSession)
+  .use(getDatabase)
+  .get<DatabaseRequest, NextApiResponse>(async (req, res) => {
+    let typeNumber = parseInt(req.query.typeNumber as string, 0);
+
+    let character: Character | null | undefined =
+      await req.db.characters?.findOne(
+        {
+          unique_id: typeNumber as number,
+        } as Filter<Character>,
+        { projection: { _id: 0 } } as FindOptions
+      );
 
     if (character) {
-        let relatedUniqueIds: number[] = [character?.good || 0, character.bad || 0]
+      let relatedUniqueIds: number[] = [
+        character?.good || 0,
+        character.bad || 0,
+      ];
 
-        let [ relatedCharacters, statistics ] = await Promise.all([
-            req.db.characters?.aggregate([
-                {$match: {unique_id: {$in: relatedUniqueIds}}},
-                {$project: {unique_id: 1, name: 1, image: 1, _id: 0}}
-            ]).toArray(),
-            req.db.statistics?.findOne({}, {projection: {_id: 0}} as FindOptions)
-        ])
+      let [relatedCharacters, statistics] = await Promise.all([
+        req.db.characters
+          ?.aggregate([
+            { $match: { unique_id: { $in: relatedUniqueIds } } },
+            { $project: { unique_id: 1, name: 1, image: 1, _id: 0 } },
+          ])
+          .toArray(),
+        req.db.statistics?.findOne({}, {
+          projection: { _id: 0 },
+        } as FindOptions),
+      ]);
 
-        // 잘 맞는 캐릭터와 아닌 캐릭터
-        let good = relatedCharacters?.find(c => character?.good == c.unique_id)
-        let bad = relatedCharacters?.find(c => character?.bad == c.unique_id)
+      // 잘 맞는 캐릭터와 아닌 캐릭터
+      let good = relatedCharacters?.find((c) => character?.good == c.unique_id);
+      let bad = relatedCharacters?.find((c) => character?.bad == c.unique_id);
 
-        // 통계적으로 몇 %?
-        let column = `type${typeNumber}`
-        let percentage: number = 100
-        
-        if (statistics) {
-            let target = (statistics[column] || 0) + 1 // 자기자신
-            let sum = Object.values(statistics).reduce((p, c) => p + c)
-            percentage = target / sum * 100
-        }
+      // 통계적으로 몇 %?
+      let column = `type${typeNumber}`;
+      let percentage: number = 100;
 
-        await updateStatistics(req, column)
-        
-        res.status(200).json({ 
-            ...character,
-            good,
-            bad,
-            percentage
-        });
+      if (statistics) {
+        let target = (statistics[column] || 0) + 1; // 자기자신
+        let sum = Object.values(statistics).reduce((p, c) => p + c);
+        percentage = (target / sum) * 100;
+      }
+
+      await updateStatistics(req, column);
+
+      res.status(200).json({
+        ...character,
+        good,
+        bad,
+        percentage,
+      });
     } else {
-        res.status(404).json({
-            code: 404,
-            message: "캐릭터를 찾을 수 없어요."
-        })
+      res.status(404).json({
+        code: 404,
+        message: "캐릭터를 찾을 수 없어요.",
+      });
     }
-})
+  });
 
 async function updateStatistics(req: DatabaseRequest, typeColumn: string) {
-    // TODO: redis에서 session uuid 별로 체크해서 increment하기
-    await req.db.statistics?.updateOne({}, { $inc: {[typeColumn]: 1 }}, {upsert: true})
+  // TODO: redis에서 session uuid 별로 체크해서 increment하기
+  await req.db.statistics?.updateOne(
+    {},
+    { $inc: { [typeColumn]: 1 } },
+    { upsert: true }
+  );
 }
 
-export default handler
+export default handler;
