89ec275abdfd1ab6c815128195e1fafc50fddaa1
Lou Park <gold24park@gmail.com>
2022-08-13 17:12:43 +0900

    modified twitter tags

--- src/components/layout/Layout.tsx
+++ src/components/layout/Layout.tsx
@@ -17,17 +17,19 @@ export const Layout = (props: Props) => {
       <Head>
         <title>김래일의 애니캐 테스트</title>
         <meta name="description" content={description} />
-        <meta name="og:url" content={process.env.NEXT_PUBLIC_URL}></meta>
-        <meta name="og:title" content={title}></meta>
-        <meta name="og:image" content={metaImage}></meta>
+        <meta property="og:url" content={process.env.NEXT_PUBLIC_URL}></meta>
+        <meta property="og:title" content={title}></meta>
+        <meta property="og:image" content={metaImage}></meta>
         <meta name="color-scheme" content="light"/>
         <meta name="supported-color-schemes" content="light"/>
         <meta name="twitter:card" content="summary_large_image" />
-        <meta name="twitter:image:src" content="/images/meta.png" />
-        <meta name="twitter:site" content={process.env.NEXT_PUBLIC_URL} />
+        <meta name="twitter:image" content={metaImage} />
+        <meta name="twitter:site" content="@rail_studio" />
+        <meta name="twitter:creator" content="@rail_studio"/>
         <meta name="twitter:title" content={title} />
         <meta name="twitter:description" content={description} />
-        <meta name="twitter:creator" content="김래일"/>
+        <meta name="twitter:title" content={title} />
+        <meta property="twitter:domain" content={process.env.NEXT_PUBLIC_URL}/>
         <link rel="icon" href="/images/favicon.ico" />
         <link href="https://fonts.googleapis.com/css2?family=Limelight&display=swap" rel="stylesheet"></link>
       </Head>
