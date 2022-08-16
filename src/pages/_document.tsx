5c63460909077c9d24e5994ed4551273faaf9bfb
lx5475 <gold24park@gmail.com>
2022-08-17 06:06:16 +0900

    added Adsense Codes

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -45,6 +45,7 @@ export default class MyDocument extends Document {
           />
           <link rel="preconnect" href="https://fonts.googleapis.com"></link>
           <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="true"></link>
+          <script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-6387136497419791" crossOrigin="anonymous"></script>
         </Head>
         <body>
           <Main />
